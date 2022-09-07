e2b1827b7abeb25c3eab7ca591db39cf591072f1
beforeold <ibeforeold@gmail.com>
2022-09-07 17:12:59 +0800

    add sqlite ios demo

---
+++ sources/TestSQLite/TestSQLite/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestSQLite
+//
+//  Created by beforeold on 2022/9/7.
+//
+
+import UIKit
+import SQLite3
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
